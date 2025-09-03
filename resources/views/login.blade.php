c383d6a4af6a96ea3d0a75c2c6f66b8658dbf3e9
Daniel Speichert <DSpeichert@users.noreply.github.com>
2025-09-03 16:45:01 -0400

    Update redirect URL in authenticate-passkey component

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,4 @@
-<x-authenticate-passkey redirect="/admin">
+<x-authenticate-passkey redirect="{{ filament()->getCurrentPanel()->getUrl() }}">
     <x-filament::button icon="heroicon-o-key" color="gray" class="w-full" onclick="authenticateWithPasskey()">
         {{ __('passkeys::passkeys.authenticate_using_passkey') }}
     </x-filament::button>
