5f03c6989c2207a17fb5296a7c7941702c3e23d6
MarcelWeidum <9413586+MarcelWeidum@users.noreply.github.com>
2026-02-09 10:57:58 +0000

    Fix styling

--- src/PasskeysPlugin.php
+++ src/PasskeysPlugin.php
@@ -10,8 +10,6 @@ use Filament\Panel;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
 use Illuminate\View\View;
-use Livewire\Livewire;
-use MarcelWeidum\Passkeys\Livewire\Passkeys as LivewirePasskeys;
 
 final class PasskeysPlugin implements Plugin
 {
