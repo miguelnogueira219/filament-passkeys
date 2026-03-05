e444002266191ae9ae44217ebfd5e22f0a6a17ad
Hamza Mohammed <132139779+hamza200411@users.noreply.github.com>
2026-03-05 20:36:53 +0300

    add Arabic Language

---
+++ resources/lang/ar/passkeys.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'authenticate_using_passkey' => __('passkeys::passkeys.authenticate_using_passkey'),
+    'create' => __('passkeys::passkeys.create'),
+    'delete' => __('passkeys::passkeys.delete'),
+    'error_something_went_wrong_generating_the_passkey' => __('passkeys::passkeys.error_something_went_wrong_generating_the_passkey'),
+    'invalid' => __('passkeys::passkeys.invalid'),
+    'last_used' => __('passkeys::passkeys.last_used'),
+    'name' => __('passkeys::passkeys.name'),
+    'name_placeholder' => __('passkeys::passkeys.name_placeholder'),
+    'no_passkeys_registered' => __('passkeys::passkeys.no_passkeys_registered'),
+    'not_used_yet' => __('passkeys::passkeys.not_used_yet'),
+    'passkeys' => __('passkeys::passkeys.passkeys'),
+    'description' => 'تتيح لك مفاتيح المرور تسجيل الدخول دون الحاجة إلى كلمة مرور. بدلاً من كلمة المرور، يمكنك إنشاء مفتاح مرور سيتم تخزينه في 1Pass أو تطبيق كلمات المرور في MacOS أو تطبيق بديل على نظام التشغيل المفضل لديك.',
+    'created_notification_title' => 'تم إنشاء مفتاح المرور الخاص بك',
+    'deleted_notification_title' => 'تم حذف مفتاح المرور الخاص بك',
+];
