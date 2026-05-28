c9e378e59aeceff48ec3017f946e2fceefded53d
MarcelWeidum <marcelweidum@live.nl>
2026-05-28 19:16:36 +0200

    fix: login url

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,10 @@
-<div x-data>
+<div x-data="{ loginUrl: @js(filament()->getCurrentOrDefaultPanel()->getUrl()) }">
     <x-filament::button
         type="button"
         icon="heroicon-o-key"
         color="gray"
         class="w-full"
-        x-on:click="window.FilamentPasskeys.login(@js(filament()->getCurrentOrDefaultPanel()->getUrl()))"
+        x-on:click="window.FilamentPasskeys.login(loginUrl)"
     >
         {{ __('filament-passkeys::passkeys.authenticate_using_passkey') }}
     </x-filament::button>
